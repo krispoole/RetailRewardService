e65c17f4aac4d737d3b55c9a15feef4cf8574650
Kris Poole <krispoole@pm.me>
2023-06-11 08:25:16 -0500

    initial commit

---
+++ src/main/java/com/krispoole/RetailRewardService/RetailRewardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.krispoole.RetailRewardService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailRewardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailRewardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krispoole/RetailRewardService/RetailRewardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krispoole.RetailRewardService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailRewardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
